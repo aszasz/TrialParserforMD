71e7e2d8e1474f04972de2a8623e58308c5bbc8c
Arthur Szasz <arthur@elebeta.com.br>
2015-12-18 02:25:51 -0200

    add spectrial still as is

--- Brtparse.hx
+++ Brtparse.hx
@@ -56,11 +56,12 @@ typedef TableColumn = Array<String>
 typedef TableLine = Array<TableColumn>
 
 enum BookElement {
-    BookDiv(name:Null<String>, isNumbered:Bool, content:Array<BookElement>);
-    BookFigure(name:Null<String>, isNumbered:Bool, content:FigureContents);
-    BookTable(name:Null<String>, isNumbered:Bool, content:Array<TableLine>);
-    BookEquation(name:Null<String>, isNumbered:Bool, content:Array<SourceText>);
-    BookText(name:Null<String>,  content:Array<SourceText>);
+    Div(name:Null<String>, isNumbered:Bool, content:Array<BookElement>);
+    Box(name:Null<String>, isNumbered:Bool, content:Array<BookElement>);
+    Figure(name:Null<String>, isNumbered:Bool, content:FigureContents);
+    Table(name:Null<String>, isNumbered:Bool, content:Array<TableLine>);
+    Equation(name:Null<String>, isNumbered:Bool, content:Array<SourceText>);
+    Text(name:Null<String>,  content:Array<SourceText>);
 }
 
 class Brtparse
@@ -75,42 +76,77 @@ class Brtparse
             displayUsageAndExit();
         } 
         if (!FileSystem.exists(Sys.args()[0])){
-            trace ('file not found:' + Sys.args()[0]);
+            trace('file not found:' + Sys.args()[0]);
             displayUsageAndExit();
         }
         trace('reading from  ${Sys.args()[0]} and writting to ${Sys.args()[1]}');
         var vBlocks = ParseIntoVBlocks (Sys.args()[0]);
         trace ('Done spliting file in vertical blocks. It has ${vBlocks.length} vBlocks.');
-        promptTestParsedParagraphs(vBlocks,Sys.args()[1]);
-//        var book = CreateBookDiv ("BRTPG", 0, false, ParseIntoDivs(parast));
+//        promptTestParsedParagraphs(vBlocks,Sys.args()[1]);
+        var book = createBookDiv ("BRTPG", 0, false, vBlocks);
    }
 
-//    public static function CreateBookDiv (name:Null<String>, level:Int, isNumbered:Bool, parast:<Array<Array<TextChunk>>):BookElement
-//    {
-//        while (parast.length>0) {
-//            par = parast.shift()
-//            if IsDivOper(par)
-//                var div=BookDiv(divName(par),ParseintoDivs(parast))
-//            else {
-//                EltoDiv(
-//            }
-//       for (par in prebook){
-//
-//
-//           if (IsNewDiv (par)) curDiv = 
-//       }
-//
-//   }
-//
-//    public static function EltoDiv(div:BookElement,bel:BookElement):Bool {
-//        switch div{
-//            case BookDiv(_,cont):
-//                content.push(bel);
-//                return true;
-//            default:
-//                return false;
-//        }
-//    }
+    public static function createBookDiv (name:Null<String>, level:Int, isNumbered:Bool, isBox:Bool, vBlocks:Array<VBlock>):BookElement
+    {
+        var content:Array<BookElements>=[];
+        while (vBlocks.length>0) {
+            var vBlock = vBlocks.shift()
+            if (vBlock[0].textContent.startsWith() == "#" ){
+                if (isBox) {
+                    trace ('try to add div inside block $name \n $displayBlockInfo(vBlock)' );
+                    Sys.exit(1);
+                }
+                var divinfo = getDivInfo(vBlock);
+                divlevel=0;
+                while 
+            
+            } 
+            if (!vBlock[0].textContent.startsWith() == "[[[" ) {
+            
+            
+            }    
+                if (!eltoDiv(myself, {null,vBlock} )) {
+                    trace ('failed to add vBlock argument to div \n $displayBlockInfo(vBlock)' );
+                    Sys.exit(2);
+                }
+                continue;
+            }
+            var div = parseDivHeader(vblock)
+            if (divLevel>=level){
+                vBlocks.unshift(vBlock);
+                if (isBox)
+                    return Box(name,isNumbered, content);
+                else
+                    return Div(name, isNumbered, content);
+
+            } else {
+            if (!isDivHeader(vBlock))
+                if (!eltoDiv(myself, createBookDiv(div.name,div.level,div.isNumbered,vBlocks)) {
+                    trace ('failed to add vBlock argument to div \n $displayBlockInfo(vBlock)' );
+                    Sys.exit(2);
+            }
+       
+    static function getDivInfo(vBlock:VBlock):{name:String, level:Int, isNumbered:Bool}
+    {
+        
+        var notnumberedCommand = {description:"In a div header to inform that the div shall not be counted in the numbering tree", nArgs:0, matchPattern:~/()\\notnumbered ()/i};
+        for (sourceText in vBlock){
+            if (notnumberedCommand.matchPattern.match(sourceText.textContent)){
+                
+                sourceText.textContent = notnumberedCommand.matchPattern.matched(1)  ;
+            }
+        } 
+
+    }
+    public static function eltoDiv(div:BookElement,bel:BookElement):Bool {
+        switch div{
+            case BookDiv(_,cont):
+                content.push(bel);
+                return true;
+            default:
+                return false;
+        }
+    }
 
     // A Vertical Block (VBlock) is text between aparently empty lines (only tabs spaces and CharCodes between 9 and 13)
     // Lines starting with % are ignored 
@@ -187,22 +223,28 @@ class Brtparse
     //Just saving intitial manual intitial testing code (not for frinedly use)
     static function promptTestParsedParagraphs(bookBlocks:Array<VBlock>,?filePath:Null<String>)
     {
-        trace('\n Enter paragraph to display (0 for exit)' );
-        var input = Std.parseInt(Sys.stdin().readLine());
-        while (input!=0){
-            var parcontent = "" ;
-            var firstline = Math.POSITIVE_INFINITY;
-            var lastline = Math.NEGATIVE_INFINITY;
-            for (sourceText in bookBlocks[input-1]) {
-                parcontent = parcontent + sourceText.textContent;
-                firstline = Math.min(firstline, sourceText.line);
-                lastline = Math.max(lastline, sourceText.line);
-            }
-            trace ("\n " + parcontent
-                    + "\n lines:" + firstline + "-" + lastline
-                    + "\n Enter new paragraph to display (0 for saveContent)" );
-            input = Std.parseInt(Sys.stdin().readLine());
+        while (true){
+            trace ('\n Enter new paragraph to display (0 for saveContent)' );
+            var input = Std.parseInt(Sys.stdin().readLine());
+            if (input==0) break;
+            trace (displayVBlockInfo(bookBlocks[input-1]);
         }
         File.saveContent(filePath,Serializer.run(bookBlocks));
     }
+
+    static function displayVBlockInfo (vBlock:VBlock, ?):Null<String>
+    {
+        if (vBlock.length == 0) return null
+        var vBContent = "" 
+        var firstline = Math.POSITIVE_INFINITY;
+        var lastline = Math.NEGATIVE_INFINITY;
+        var fileName = vBlock[0].fileName;
+        for (sourceText in vBlock) {
+            vBcontent = vBcontent + sourceText.textContent;
+            firstline = Math.min(firstline, sourceText.line);
+            lastline = Math.max(lastline, sourceText.line);
+        }
+        return ('\n from $fileName lines: $firstline - $lastline --> $vbContent');
+    }
+
 }
